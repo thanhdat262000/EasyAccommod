69596c86a15a4364c18b149f36d45dc67802e1ec
Nguyễn Thành đạt <thanhdat262000@gmail.com>
2020-11-09 20:08:44 +0700

    update-home

---
+++ Frontend/src/components/layout/background.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "../../css/background.css";
+export default class Background extends Component {
+  render() {
+    return (
+      <div className="background">
+        <div className="overlay">
+          <div className="title">
+            <h1>EasyAccommod.com</h1>
+            <h2>Trang web thuê nhà hàng đầu Việt Nam</h2>
+          </div>
+          <div className="buttons">
+            <button id="post">Đăng tin</button>
+            <button id="search">Tìm kiếm</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
