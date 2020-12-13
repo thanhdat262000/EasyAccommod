856bfd46c3b66a5ed55dd3f9a2e3ca7eddc4866e
Nam Nguyen <nguyendangnam257@gmail.com>
2020-12-13 23:39:39 +0700

    Add signin feature

--- Backend/controllers/signin.js
+++ Backend/controllers/signin.js
@@ -5,12 +5,12 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
-module.exports.signinPost = (req, res) => {
+module.exports.signinPost = async(req, res) => {
     let {email ,password} = req.body;
-    let sql = "SELECT password, account_name, privilege_id FROM account WHERE account_name = ?";
+    let sql = "SELECT password, email, privilege FROM account WHERE email = ?";
     connection.query(sql, [email], (err, results, fields) => {
         if(err) res.sendStatus(404);
-        if(password.length === 0){
+        if(results[0].email.length === 0){
             res.send("You must register if you want to signin")
         }
         else{
@@ -18,15 +18,14 @@ module.exports.signinPost = (req, res) => {
             bcrypt.compare(password, results[0].password, function(err, result) {
                 // if result == true, password matched
                 // else wrong password
-                console.log(results);
-                console.log(password);
+                // console.log(results);
+                // console.log(password);
                 if(result){
                     console.log(results[0]);
 
                     // Create token by jwt
                     const token = jwt.sign({
-                        "user": results[0].account_name,
-                        "privilege": results[0].privilege_id
+                        data: results[0].email + ' ' + results[0].privilege
                     }, process.env.JWT_KEY, {expiresIn: 60*30});
 
                     // Send token
