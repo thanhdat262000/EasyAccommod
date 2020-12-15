fa08d904d00a91b276c9c37f4229c275c02020e3
Nam Nguyen <nguyendangnam257@gmail.com>
2020-12-15 17:44:56 +0700

    Fix get all apartments

--- Backend/controllers/apartmentsController.js
+++ Backend/controllers/apartmentsController.js
@@ -2,7 +2,7 @@ const connection = require('../db');
 
 // Send all apartments
 module.exports.index = async(req, res) => {
-    const sql = `SELECT apartment.apartment_id, apartment_detail.price, city.name, district.name 
+    const sql = `SELECT apartment.apartment_id, apartment_detail.price, city.name AS city, district.name AS district
         FROM apartment 
         JOIN apartment_detail ON apartment.apartment_id = apartment_detail.apartment_id 
         JOIN city ON apartment.city_id = city.city_id 
