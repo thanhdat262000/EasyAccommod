87b206020bedbfb7a279b90d893cc31163880803
Nam Nguyen <nguyendangnam257@gmail.com>
2020-12-17 23:25:22 +0700

    Done search apartments

--- Backend/controllers/apartments/apartmentsController.js
+++ Backend/controllers/apartments/apartmentsController.js
@@ -34,8 +34,8 @@ module.exports.index = async(req, res) => {
     }
     else{
         const {
-            city_id,
-            district_id,
+            city_name,
+            district_name,
             apartment_type,
             rent_min,
             rent_max,
@@ -43,23 +43,38 @@ module.exports.index = async(req, res) => {
             square_max,
           } = req.query;
           
-          const sql = `SELECT apartment.apartment_id, apartment_detail.price, city.name AS city, district.name AS district
+          const sql = `SELECT apartment.apartment_id, apartment_detail.price, city.name AS city, district.name AS district, apartment_detail.square AS square
           FROM apartment
           JOIN apartment_detail ON apartment.apartment_id = apartment_detail.apartment_id
           JOIN city ON apartment.city_id = city.city_id 
           JOIN district ON district.city_id = city.city_id
-          WHERE apartment.city_id = ?
-            AND apartment.district_id = ?
-            AND apartment.apartment_type = ?
-            AND (apartment_detail.price BETWEEN ? AND ?) 
-            AND (apartment_detail.square BETWEEN ? AND ?)`
-        connection.query(sql, [city_id, district_id, apartment_type, rent_min, rent_max, square_min, square_max] , (err, results, fields) => {
+          WHERE city.name = ?
+            AND district.name = ?
+            AND apartment.apartment_type = ?`
+        console.log(city_name, district_name, apartment_type)
+        console.log(typeof(rent_min))
+        connection.query(sql, [city_name, district_name, apartment_type] , (err, results, fields) => {
             try{
                 if(err) throw err;
                 // console.log(req.url);
                 // req.url = `/`
                 // console.log(req.url);
-                res.json(results);
+                const data = results.filter(apartment => {
+                    if(rent_min.length === 0){
+                        if(rent_max.length === 0){
+                            if(square_min.length === 0){
+                                if(square_max.length === 0){
+                                    return true;
+                                }
+                                return apartment.square <= square_max;
+                            }
+                            return apartment.square <= square_max && apartment.square >= square_min;
+                        }
+                        return apartment.square <= square_max && apartment.square >= square_min && apartment.price <= rent_max;
+                    }
+                    return apartment.square <= square_max && apartment.square >= square_min && apartment.price <= rent_max && apartment.price >= rent_min;
+                })
+                res.json(data);
             }catch(err){
                 console.log(err);
                 res.sendStatus(400)
