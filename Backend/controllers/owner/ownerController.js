aee7692e5ce05246c829297209b57deee44995c7
Nam Nguyen <nguyendangnam257@gmail.com>
2020-12-19 17:53:10 +0700

    Create APIs: approved, rented, expired owner

--- Backend/controllers/owner/ownerController.js
+++ Backend/controllers/owner/ownerController.js
@@ -13,3 +13,45 @@ module.exports.getAllPending = async (req, res) => {
         res.json(results);
     })
 }
+
+module.exports.getAllApproved = async (req, res) => {
+    const sql = `SELECT apartment.status, apartment.expiration, apartment_detail.square, apartment_detail.price, city.name AS city, district.name AS district
+    FROM apartment
+    JOIN apartment_detail ON apartment.apartment_id = apartment_detail.apartment_id
+    JOIN city ON city.city_id = apartment.city_id
+    JOIN district ON district.city_id = city.city_id
+    WHERE apartment.status = 'Đã được duyệt' AND apartment.account_id = ?`;
+
+    connection.query(sql,[req.id], (err, results, fields) => {
+        if(err) return res.sendStatus(404);
+        res.json(results);
+    })
+}
+
+module.exports.getAllRented = async (req, res) => {
+    const sql = `SELECT apartment.status, apartment.expiration, apartment_detail.square, apartment_detail.price, city.name AS city, district.name AS district
+    FROM apartment
+    JOIN apartment_detail ON apartment.apartment_id = apartment_detail.apartment_id
+    JOIN city ON city.city_id = apartment.city_id
+    JOIN district ON district.city_id = city.city_id
+    WHERE apartment.status = 'Đã thuê' AND apartment.account_id = ?`;
+
+    connection.query(sql,[req.id], (err, results, fields) => {
+        if(err) return res.sendStatus(404);
+        res.json(results);
+    })
+}
+
+module.exports.getAllExpired = async (req, res) => {
+    const sql = `SELECT apartment.status, apartment.expiration, apartment_detail.square, apartment_detail.price, city.name AS city, district.name AS district
+    FROM apartment
+    JOIN apartment_detail ON apartment.apartment_id = apartment_detail.apartment_id
+    JOIN city ON city.city_id = apartment.city_id
+    JOIN district ON district.city_id = city.city_id
+    WHERE apartment.status = 'Đã hết hạn' AND apartment.account_id = ?`;
+
+    connection.query(sql,[req.id], (err, results, fields) => {
+        if(err) return res.sendStatus(404);
+        res.json(results);
+    })
+}
--- Backend/routes/owner.js
+++ Backend/routes/owner.js
@@ -9,4 +9,10 @@ const ownerRouter = express.Router();
 
 ownerRouter.get('/apartments/pending', authUser, authRole(ROLE.OWNER), ownerController.getAllPending)
 
+ownerRouter.get('/apartments/approved', authUser, authRole(ROLE.OWNER), ownerController.getAllApproved)
+
+ownerRouter.get('/apartments/rented', authUser, authRole(ROLE.OWNER), ownerController.getAllRented)
+
+ownerRouter.get('/apartments/expired', authUser, authRole(ROLE.OWNER), ownerController.getAllExpired)
+
 module.exports = ownerRouter;
