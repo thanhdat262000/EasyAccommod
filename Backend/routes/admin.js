5f5dbdc541955cc4bf7b2647fed233ae1f116758
Nam Nguyen <nguyendangnam257@gmail.com>
2020-12-21 00:27:02 +0700

    Create APIs apartments with admin

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -7,10 +7,22 @@ const { ROLE } = require('../role');
 
 const adminRouter = express.Router();
 
-adminRouter.get('/', authUser, authRole(ROLE.ADMIN),adminController.index)
-
-adminRouter.get('/allOwners', authUser, authRole(ROLE.ADMIN), adminController.getAllOwners);
+adminRouter.get('/allOwners', adminController.getAllOwners);
 
 adminRouter.get('/apartments-posts', adminController.getAllApartmentPost);
+
+adminRouter.post('/apartment-post', adminController.postApartment);
+
+adminRouter.put('/apartments/:id', adminController.putEditApartment);
+
+adminRouter.put('/apartments/:id/rented', adminController.putChangeRented);
+
+adminRouter.put('/apartments/:id/cancel', adminController.putChangeCancel);
+
+adminRouter.get('/apartments/approved', adminController.getAllApproved);
+
+adminRouter.get('/apartments/rented', adminController.getAllRented);
+
+adminRouter.get('/apartments/expired', adminController.getAllExpired);
 
 module.exports = adminRouter;
