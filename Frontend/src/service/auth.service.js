4b68e62137f35fa5695aa16dfae3f4df33a76462
Nguyễn Thành đạt <thanhdat262000@gmail.com>
2020-12-23 00:37:36 +0700

    update-chat

--- Frontend/src/service/auth.service.js
+++ Frontend/src/service/auth.service.js
@@ -22,6 +22,7 @@ export const login = async (data) => {
 };
 export const logout = () => {
   localStorage.removeItem("token");
+  window.location.reload(false);
 };
 export const saveToken = (token) => {
   localStorage.setItem("token", token);
